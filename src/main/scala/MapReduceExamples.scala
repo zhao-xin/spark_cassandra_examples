6c71cf99978fbdbbcf1ec0f9ea71c9c6f2025aea
oonlyo <oonlyo@gmail.com>
2018-07-28 22:41:24 +1000

    update jar

--- src/main/scala/MapReduceExamples.scala
+++ src/main/scala/MapReduceExamples.scala
@@ -14,23 +14,24 @@ object MapReduceExamples {
   def main(args: Array[String]): Unit = {
 
     // turn off chatty off for local development. DO NOT do this in production
-//    Logger.getLogger("org").setLevel(Level.OFF)
-//    Logger.getLogger("akka").setLevel(Level.OFF)
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
 
     val appName = "MapReduceExamples"
 
     /////////////////// local debug //////////////////
-//    val sparkMaster = "local"/
-//    val cassandraHost = "cassandra1.zones.eait.uq.edu.au,cassandra2.zones.eait.uq.edu.au,cassandra3.zones.eait.uq.edu.au"
-//    val conf = new SparkConf(true)
-//      .set("spark.cassandra.connection.host", cassandraHost)
-//    val sc = new SparkContext(sparkMaster, appName, conf)
+    val sparkMaster = "local"
+    val cassandraHost = "cassandra1.zones.eait.uq.edu.au,cassandra2.zones.eait.uq.edu.au,cassandra3.zones.eait.uq.edu.au"
+    val conf = new SparkConf(true)
+      .set("spark.cassandra.connection.host", cassandraHost)
+    val sc = new SparkContext(sparkMaster, appName, conf)
 
     /////////////////// spark-submit job ///////////////////
-    val conf = new SparkConf(true).setAppName(appName)
+//    val conf = new SparkConf(true).setAppName(appName)
+//    val sc = new SparkContext(conf)
     /////////////////// init spark cassandra connector ///////////////////
     val keySpace = "cloudcomputing"
-    val sc = new SparkContext(conf)
+
 
     val table = "data"
 
@@ -44,11 +45,13 @@ object MapReduceExamples {
     }.reduceByKey((a,b) => (a._1+b._1, a._2+b._2))
       .map{case (date, (count, sum)) => (date, sum/count)}
 
+    println()
     rdd3.foreach(println)
     println()
 
     // all records in campus "St Lucia"
     val rdd1 = cassTable.collect
+
     println()
     rdd1.foreach(println)
     println()
@@ -58,6 +61,7 @@ object MapReduceExamples {
       case (date, sensor_id, temperature) => (1, temperature)
     }.reduce((a,b) => (a._1+b._1, a._2+b._2))
 
+    println()
     println("avg_temp: " + rdd2._2/rdd2._1)
     println()
 
