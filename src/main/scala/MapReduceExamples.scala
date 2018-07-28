f6a9d8af15d70996f64b49c04362e489d5bbeca4
oonlyo <oonlyo@gmail.com>
2018-07-28 22:21:18 +1000

    update jar

--- src/main/scala/MapReduceExamples.scala
+++ src/main/scala/MapReduceExamples.scala
@@ -14,13 +14,13 @@ object MapReduceExamples {
   def main(args: Array[String]): Unit = {
 
     // turn off chatty off for local development. DO NOT do this in production
-    Logger.getLogger("org").setLevel(Level.OFF)
-    Logger.getLogger("akka").setLevel(Level.OFF)
+//    Logger.getLogger("org").setLevel(Level.OFF)
+//    Logger.getLogger("akka").setLevel(Level.OFF)
 
     val appName = "MapReduceExamples"
 
-    /////////////////// local debug ///////////////////
-//    val sparkMaster = "local"
+    /////////////////// local debug //////////////////
+//    val sparkMaster = "local"/
 //    val cassandraHost = "cassandra1.zones.eait.uq.edu.au,cassandra2.zones.eait.uq.edu.au,cassandra3.zones.eait.uq.edu.au"
 //    val conf = new SparkConf(true)
 //      .set("spark.cassandra.connection.host", cassandraHost)
@@ -28,15 +28,21 @@ object MapReduceExamples {
 
     /////////////////// spark-submit job ///////////////////
     val conf = new SparkConf(true).setAppName(appName)
-    val sc = new SparkContext(conf)
-
     /////////////////// init spark cassandra connector ///////////////////
     val keySpace = "cloudcomputing"
+    val sc = new SparkContext(conf)
+
     val table = "data"
 
     val cassTable = sc.cassandraTable[(String, Int, Float)](keySpace, table).select("date", "sensor_id", "temperature").where("campus = ?", "St Lucia" );
 
     ///////////////////// map reduce ///////////////////
+
+    // average temperature of each day in campus "St Lucia"
+    val rdd3 = cassTable.map{
+      case (date, sensor_id, temperature) => (date, (1, temperature))
+    }.reduceByKey((a,b) => (a._1+b._1, a._2+b._2))
+      .map{case (date, (count, sum)) => (date, sum/count)}
 
     // all records in campus "St Lucia"
     val rdd1 = cassTable.collect
@@ -51,12 +57,6 @@ object MapReduceExamples {
 
     println("avg_temp: " + rdd2._2/rdd2._1)
     println()
-
-    // average temperature of each day in campus "St Lucia"
-    val rdd3 = cassTable.map{
-      case (date, sensor_id, temperature) => (date, (1, temperature))
-    }.reduceByKey((a,b) => (a._1+b._1, a._2+b._2))
-      .map{case (date, (count, sum)) => (date, sum/count)}
 
     rdd3.foreach(println)
     println()
