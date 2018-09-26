21182ca5181b8b5d5f35981e20b1079854fcfa8a
FDX\jonathan.zhao <jonathan.zhao@fonedynamics.com.au>
2018-09-26 16:15:02 +1000

    improve php example

--- docker/phpcass/src/index.php
+++ docker/phpcass/src/index.php
@@ -1,22 +1,38 @@
-<?php 
-error_reporting(-1);
-ini_set('display_errors', 'On');
-echo "Hello World (php)\n"; 
-?>
 <?php
-$cluster   = Cassandra::cluster()
- ->withContactPoints('172.23.99.231')
-               ->withPort(9042)                 // connects to localhost by default
-                 ->build();
-$keyspace  = 'cloudcomputing';
-$session   = $cluster->connect($keyspace);        // create session, optionally scoped to a keyspace
-$statement = new Cassandra\SimpleStatement(       // also supports prepared and batch statements
-    'SELECT * FROM data'
-);
-$future    = $session->executeAsync($statement);  // fully asynchronous and easy parallel execution
-$result    = $future->get();                      // wait for the result, with an optional timeout
-
-foreach ($result as $row) {                       // results and rows implement Iterator, Countable and ArrayAccess
-    printf("%s\n", implode("|",$row));
-}
+    // enable php debug
+    error_reporting(-1);
+    ini_set('display_errors', 'On');
 ?>
+
+<html>
+    <head>
+        <title>Cloud computing Prac 8</title>
+    </head>
+    <body>
+        <h1>
+            <?php
+                echo "sensor records of St Lucia\n";
+            ?>
+        </h1>
+
+        <?php
+            $cluster = Cassandra::cluster()
+                ->withContactPoints('172.17.0.3','172.17.0.4') // cassandra address 
+                ->withPort(9042)
+                ->build();
+            $keyspace = 'cloudcomputing'; // keyspace
+            $session = $cluster->connect($keyspace);
+            $statement = new Cassandra\SimpleStatement(
+                "SELECT * FROM data WHERE campus = 'St Lucia'" // cql sentence
+            );
+            $future = $session->executeAsync($statement); // fully asynchronous and easy parallel execution
+            $result = $future->get(); // wait for the result, with an optional timeout
+
+            echo "<table>";
+            foreach ($result as $row) { // results and rows implement Iterator, Countable and ArrayAccess
+                    echo "<tr><td>" . implode("|",$row)  .  "</td></tr>";
+            }
+            echo "</table>";
+        ?>
+    </body>
+</html>
